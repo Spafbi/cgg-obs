63adbd2983f35a38441f41d907b5a25871419008
Spafbi <spafbi@outlook.com>
2023-06-19 20:05:02 -0500

    Start moving to class. Download latest.

--- setup-cgg-obs.py
+++ setup-cgg-obs.py
@@ -1,10 +1,11 @@
+from bs4 import BeautifulSoup
+from fnmatch import fnmatch
 from glob import glob
 from os.path import abspath, basename, dirname
 from pathlib import Path
+from pprint import pprint
 from win32com.client import Dispatch
 import argparse
-
-# import ctypes
 import hashlib
 import inspect
 import json
@@ -20,6 +21,243 @@ import winshell
 import zipfile
 
 sys.path.insert(1, abspath(dirname(sys.argv[0])))
+
+
+class cggOBS:
+    def __init__(self, **kwargs):
+        # Get some vars from the json_data
+        self.api_key = kwargs.get("api_key")
+        self.downloads = kwargs.get("downloads")
+        self.downloads_directory = kwargs.get("downloads_directory")
+        self.icon = kwargs.get("icon")
+        self.install_log = kwargs.get("install_log")
+        self.installation_directory = kwargs.get("installation_directory")
+        self.moves = kwargs.get("moves", dict())
+
+        self.makedir(self.downloads_directory)
+        self.makedir(self.installation_directory)
+
+    def makedir(self, directory):
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
+    def install_from_github(self, this_object):
+        # Get object details from config
+        this_thing = self.downloads.get(this_object, [False, False, False])
+
+        # Return False if object was not in the config
+        if not this_thing:
+            logging.debug(f"{this_object} not found in downloads configuration")
+            return
+
+        # The the github info for object downloads
+        target_url, target_filename, target_tag = get_github_project_download_url(
+            this_thing.get("github"), this_thing.get("filename"), self.api_key
+        )
+
+        # Exit if the installed tag matches the
+        if self.install_log.get(this_object, {}).get("tag", False) == target_tag:
+            logging.debug("Installed version matches latest. Skipping download.")
+            return
+
+        # Download the file and extract it
+        if download_file(target_url, self.downloads_directory, target_filename, self.api_key):
+            print_text(f"Downloading {this_object}...")
+            directory = Path(self.installation_directory)
+            file_path = Path(f"{self.downloads_directory}/{target_filename}")
+            filename = f"{file_path}"
+            filename_len = len(filename)
+            if filename[filename_len - 3 :].lower() == "zip":
+                extraction_success = extract_zip(directory, file_path)
+            elif filename[filename_len - 2 :].lower() == "7z":
+                extraction_success = extract_7z(directory, file_path)
+            else:
+                extraction_success = [False]
+            # Log success
+            if extraction_success[0]:
+                print_text(f"Installed {this_object}")
+                self.install_log[this_object] = {
+                    "filename": target_filename,
+                    "tag": target_tag,
+                }
+                return
+            else:
+                logging.info(f"Error installing {this_object}: {extraction_success[0]}")
+        else:
+            logging.info(
+                f"Could not download object: {target_url}, {self.downloads_directory}, {target_filename}"
+            )
+        return
+
+    def install_from_obsproject(self, this_object):
+        # Get object details from config
+        this_thing = self.downloads.get(this_object, [False, False, False])
+
+        # Return False if object was not in the config
+        if not this_thing:
+            logging.debug(f"{this_object} not found in downloads configuration")
+            return
+
+        # The obsproject info for object downloads
+        target_url, target_filename = get_obs_project_download_url(
+            this_thing.get("obsproject"),
+            this_thing.get("filename"),
+        )
+
+        # Exit if the installed previously installed filename matches
+        has_asterisk = "*" in this_thing.get("filename")
+        has_question_mark = "?" in this_thing.get("filename")
+        filenames_match = (
+            self.install_log.get(this_object, {}).get("filename", False)
+            == target_filename
+        )
+        force_download = not (has_asterisk or has_question_mark)
+        if filenames_match and not force_download:
+            logging.debug("Installed version matches latest. Skipping download.")
+            return
+
+        # Download the file and extract it
+        if download_file(target_url, self.downloads_directory, target_filename):
+            print_text(f"Downloading {this_object}...")
+            directory = Path(self.installation_directory)
+            file_path = Path(f"{self.downloads_directory}/{target_filename}")
+            filename = f"{file_path}"
+            filename_len = len(filename)
+            if filename[filename_len - 3 :].lower() == "zip":
+                extraction_success = extract_zip(directory, file_path)
+            elif filename[filename_len - 2 :].lower() == "7z":
+                extraction_success = extract_7z(directory, file_path)
+            else:
+                extraction_success = False
+            # Log success
+            if extraction_success[0]:
+                print_text(f"Installed {this_object}")
+                self.install_log[this_object] = {"filename": target_filename}
+                return
+            else:
+                logging.info(f"Error installing {this_object}: {extraction_success[0]}")
+        else:
+            logging.info(
+                f"Could not download object: {target_url}, {self.downloads_directory}, {target_filename}"
+            )
+        return
+
+
+def get_obs_project_download_url(obsproject_path, file_pattern):
+    logging.debug(f"\r\nExecuting function: {inspect.stack()[0][3]}")
+    logging.debug(f"  obsproject_path: {obsproject_path}")
+    logging.debug(f"     file_pattern: {file_pattern}")
+    page_url = f"https://obsproject.com/forum/resources/{obsproject_path}/download"
+    # Fetch the webpage
+    try:
+        response = requests.get(page_url)
+    except Exception as e:
+        logging.debug(e)
+        return False, False
+
+    # If the request was successful, the status code will be 200
+    if response.status_code != 200:
+        logging.debug(
+            f"Download URL not identified due to HTTP reponse code: {response.status_code}"
+        )
+        return False, False
+
+    # Get the content of the response
+    page_content = response.content
+
+    # Create a BeautifulSoup object and specify the parser
+    soup = BeautifulSoup(page_content, "html.parser")
+
+    # Find all elements with class "contentRow-title"
+    file_elements = soup.find_all(class_="contentRow-title")
+
+    # For each element, get the text and match with the pattern
+    for file_element in file_elements:
+        file_name = file_element.get_text().strip()
+        if fnmatch(file_name, file_pattern):
+            # If the filename matches the pattern, get the download link
+            download_link = file_element.find_previous(
+                "a", {"class": "button--icon--download"}
+            )
+            if download_link is not None:
+                download_url = download_link["href"]
+                # Return the full download URL, not just the path
+                return requests.compat.urljoin(page_url, download_url), file_name
+    return False, False
+
+
+def get_github_project_download_url(github_path, file_pattern, api_key=""):
+    logging.debug(f"\r\nExecuting function: {inspect.stack()[0][3]}")
+    logging.debug(f"   github_path: {github_path}")
+    logging.debug(f"  file_pattern: {file_pattern}")
+    logging.debug(f"       api_key: '{api_key}'")
+    page_url = f"https://api.github.com/repos/{github_path}/releases/latest"
+
+    headers = dict()
+    if len(api_key) > 1:
+        headers = {
+            "Authorization": f"Bearer {api_key}",
+            "X-GitHub-Api-Version": "2022-11-28",
+        }
+
+    try:
+        response = requests.get(page_url, headers=headers)
+    except Exception as e:
+        logging.debug(e)
+        return False, False, False
+
+    if response.status_code != 200:
+        logging.debug(
+            f"Download URL not identified due to HTTP reponse code: {response.status_code}"
+        )
+        return False, False, False
+
+    json_data = response.text
+    json_data = json.loads(json_data)
+
+    for asset in json_data.get("assets", {}):
+        if fnmatch(asset.get("name"), file_pattern):
+            return (
+                asset.get("browser_download_url"),
+                asset.get("name"),
+                json_data.get("tag_name"),
+            )
+
+    return False, False, False
+
+
+def read_json(json_path):
+    if not os.path.isfile(json_path):
+        logging.debug("File not found. Returning empty dictionary.")
+        return {}
+
+    try:
+        with open(json_path) as f:
+            json_data = json.load(f)
+    except Exception as e:
+        logging.debug(e)
+        logging.debug("File load error. Returning empty dictionary.")
+        json_data = {}
+    return json_data
+
+
+def get_install_log(directory):
+    logging.debug(f"\r\nExecuting function: {inspect.stack()[0][3]}")
+    logging.debug(f"  directory: {directory}")
+    json_path = Path(f"{directory}/install_log.json")
+    return read_json(json_path)
+
+
+def set_install_log(directory, log_info):
+    logging.debug(f"\r\nExecuting function: {inspect.stack()[0][3]}")
+    logging.debug(f"  directory: {directory}")
+    json_path = Path(f"{directory}/install_log.json")
+    json_object = json.dumps(log_info, indent=4)
+    try:
+        with open(json_path, "w") as outfile:
+            outfile.write(json_object)
+    except Exception as e:
+        logging.debug(f"Log file write failed: {e}")
 
 
 def print_text(
@@ -41,119 +279,28 @@ def print_text(
         print(element)
 
 
-def download_and_extract(
-    download_name, this_object, installation_directory, download_directory=False
-):
-    logging.debug(f"\r\nExecuting function: {inspect.stack()[0][3]}")
-    logging.debug(f"           download_name: {download_name}")
-    logging.debug(f"             this_object: {this_object}")
-    logging.debug(f"  installation_directory: {installation_directory}")
-    logging.debug(f"      download_directory: {download_directory}")
-    print("—" * 70)
-    print_text(f"{download_name}", "")
-    print("—" * 70)
-    download_url = this_object.get("download_url", False)
-    filename = this_object.get("filename", False)
-    md5sum = this_object.get("md5", False)
-
-    installation_path = Path(installation_directory)
-    if not download_directory:
-        download_directory_path = Path(f"{installation_directory}/downloads")
-    else:
-        download_directory_path = Path(f"{download_directory}")
-    file_path = Path(f"{download_directory_path}/{filename}")
-    file_semaphore = Path(f"{download_directory_path}/{filename}.installed")
-
-    # Create the download directory if needed
-    if not os.path.exists(download_directory_path):
-        os.makedirs(download_directory_path)
-
-    # Check to see if the file has been downloaded
-    is_downloaded = os.path.isfile(file_path)
-
-    # If the file has been downloaded compare md5 values, else set md5_matches to False
-    md5_matches = md5_check(file_path, md5sum) if is_downloaded else False
-
-    # To force a reinstallation of a package, remove the installation semaphore if the md5sum doesn't match
-    if not md5_matches:
-        try:
-            os.remove(file_semaphore)
-        except Exception as e:
-            pass
-
-    # Retrying up to three times, download the target file until the md5sum matches the expected value
-    retries = 3
-    while (not md5_matches) and (retries > 0):
-        print_text(f"File not yet downloaded or md5sum check failed")
-        print_text(f"Downloading {download_name}")
-        download_file(download_url, download_directory_path, filename)
-        md5_matches = md5_check(file_path, md5sum)
-        retries -= 1
-
-    # Exit if the md5 values don't match by this point.
-    if not md5_matches:
-        print_text("Downloaded file does not match expected checksum value")
-        print_text(
-            f"Download of {download_name} failed. {download_name} has not been installed"
-        )
-        print()
-        return False
-
-    is_installed = os.path.isfile(file_semaphore) if md5_matches else False
-
-    if md5_matches and not is_installed:
-        print_text(f"Installing {download_name}")
-        filename_len = len(filename)
-        if filename[filename_len - 3 :].lower() == "zip":
-            installed, error = extract_zip(installation_path, file_path)
-        elif filename[filename_len - 2 :].lower() == "7z":
-            installed, error = extract_7z(installation_path, file_path)
-        else:
-            installed = False
-
-        if not installed:
-            print_text(f"Installation of {download_name} failed")
-            return False, error
-        else:
-            f = open(file_semaphore, "a")
-            f.close()
-    else:
-        print_text(f"{download_name} already installed/updated")
-    print()
-    return True, False
-
-
 # Function to download a file
-def download_file(url, directory, filename):
+def download_file(url, directory, filename, api_key=""):
     logging.debug(f"\r\nExecuting function: {inspect.stack()[0][3]}")
     logging.debug(f"        url: {url}")
     logging.debug(f"  directory: {directory}")
     logging.debug(f"   filename: {filename}")
+
+    headers = dict()
+    if len(api_key) > 1:
+        headers = {
+            "Authorization": f"Bearer {api_key}",
+            "X-GitHub-Api-Version": "2022-11-28",
+        }
+
     try:
-        response = requests.get(url)
+        response = requests.get(url, headers=headers)
         with open(os.path.join(directory, filename), "wb") as f:
             f.write(response.content)
         return True
     except Exception as e:
         logging.critical(f"Failed to download {filename} from {url}")
         return False
-
-
-def md5_check(filename, expected_hash):
-    logging.debug(f"\r\nExecuting function: {inspect.stack()[0][3]}")
-    logging.debug(f"       filename: {filename}")
-    logging.debug(f"  expected_hash: {expected_hash}")
-    filename = Path(filename)
-    hash_md5 = hashlib.md5()
-
-    with open(filename, "rb") as f:
-        for chunk in iter(lambda: f.read(4096), b""):
-            hash_md5.update(chunk)
-
-    if hash_md5.hexdigest() == expected_hash:
-        return True
-
-    return False
 
 
 def extract_zip(target_directory, filename):
@@ -312,6 +459,14 @@ def main():
         default="CGG",
     )
     parser.add_argument(
+        "-g",
+        "--github",
+        type=str,
+        required=False,
+        help="""GitHub personal access token - create one with only "public_repo" permissions at: https://github.com/settings/tokens""",
+        default="",
+    )
+    parser.add_argument(
         "-v",
         "--verbose",
         action="store_true",
@@ -345,6 +500,9 @@ def main():
     else:
         cgg_obs_logger.setLevel(logging.INFO)
 
+    # Get installation log
+    install_log = get_install_log(downloads_directory)
+
     # Download the JSON content from the Gist or load from local file
     json_path = Path(args.json)
     if os.path.isfile(json_path):
@@ -356,31 +514,48 @@ def main():
             logging.debug("Configuration file load error. Using default configuration")
             json_data = {}
     else:
-        gist_url = "https://cgg.spafbi.com/cgg-obs.json"
-        response = requests.get(gist_url)
+        json_dowload_info = get_github_project_download_url(
+            "Spafbi/cgg-obs", "cgg-obs.json"
+        )
+        if json_dowload_info[0]:
+            response = requests.get(json_dowload_info[0])
+        else:
+            logging.info(
+                "Could not determine cgg-obs.json download URL for the current release. Installation halted."
+            )
+            exit(1)
+
         json_data = response.text
         json_data = json.loads(json_data)
 
-    # Get some vars from the json_data
-    data = json_data.get("downloads", dict())
+    # Create configuration dictionary
     icon = json_data.get("branding", dict()).get(args.branding, False)
     if not icon:
         icon = json_data.get("branding", dict()).get("CGG")
+
+    config = dict()
+    data = json_data.get("downloads", dict())
     moves = json_data.get("moves", dict())
+    config["api_key"] = args.github
+    config["downloads"] = data
+    config["downloads_directory"] = downloads_directory
+    config["icon"] = icon
+    config["install_log"] = install_log
+    config["installation_directory"] = installation_directory
+    config["moves"] = moves
+    installer = cggOBS(**config)
 
-    # Let's first download and extract OBS
-    this_obs = data.get("OBS", False)
-
-    if this_obs:
-        success, error = download_and_extract(
-            "OBS", this_obs, installation_directory, downloads_directory
-        )
+    # Install OBS
+    installer.install_from_github("OBS")
 
     # Iterate over the items in the JSON data
     for key, value in data.items():
         if key.lower() == "obs":
             continue
-        download_and_extract(key, value, installation_directory, downloads_directory)
+        if value.get("github", False):
+            installer.install_from_github(key)
+        elif value.get("obsproject", False):
+            installer.install_from_obsproject(key)
 
     for key, value in moves.items():
         source = Path(f"{installation_directory}/{value}")
@@ -394,15 +569,26 @@ def main():
         move_and_remove_directory(source, destination)
         print("—" * 70)
 
-    icon_filename = icon.get("filename", "cgg-rotated-logo.ico")
-    icon_url = icon.get("download_url", "https://cgg.spafbi.com/cgg-rotated-logo.ico")
-    icon_path = Path(f"{installation_directory}/{icon_filename}")
+
+    # Create a Desktop shortcut
+    icon_url, target_filename, tag_name = get_github_project_download_url(
+        icon.get("github"),
+        icon.get("filename"),
+        args.github
+    )
+    icon_path = Path(f"{installation_directory}/{target_filename}")
     shortcut_name = icon.get("shortcut_name", "Chaotic Good Gaming OBS")
 
     if not os.path.exists(icon_path):
-        download_file(icon_url, installation_directory, icon_filename)
+        download_file(icon_url, installation_directory, target_filename, args.github)
 
-    create_shortcuts(installation_directory, icon_filename, shortcut_name)
+    installer.install_log["shortcut_icon"] = {"filename": target_filename}
+
+    create_shortcuts(installation_directory, target_filename, shortcut_name)
+
+    # Write out install log
+    set_install_log(downloads_directory, installer.install_log)
+
 
 if __name__ == "__main__":
     main()
